2f30ef546a97b38baf7ebaa151945a7ae0f768b2
Christopher Eteka <christopher.eteka@nl.abnamro.com>
2024-10-08 16:34:06 +0200

    Intentionally skip ssl validation

--- src/main/java/com/chrisworks/configurations/AppConfig.java
+++ src/main/java/com/chrisworks/configurations/AppConfig.java
@@ -1,10 +1,17 @@
 package com.chrisworks.configurations;
 
+import jakarta.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
+
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.cert.X509Certificate;
 
 @Slf4j
 @Configuration
@@ -12,8 +19,28 @@ import org.springframework.web.client.RestClient;
 public class AppConfig {
 
     @Bean("tinkHttpClient")
-    public RestClient tinkHttpClient(RestClient.Builder restClientBuilder, AppConfigProperties appConfigProperties) {
+    RestClient tinkHttpClient(RestClient.Builder restClientBuilder, AppConfigProperties appConfigProperties) {
         return restClientBuilder.baseUrl(appConfigProperties.getTinkConfig().getBaseUrl()).build();
+    }
+
+    //Intentionally skip this
+    @PostConstruct
+    void disableSSLValidation() throws Exception {
+        final SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, new TrustManager[]{new X509TrustManager() {
+            @Override
+            public void checkClientTrusted(X509Certificate[] x509Certificates, String s) {}
+
+            @Override
+            public void checkServerTrusted(X509Certificate[] x509Certificates, String s) {}
+
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                return new X509Certificate[0];
+            }
+        }}, null);
+        HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
+        HttpsURLConnection.setDefaultHostnameVerifier((_, _) -> true);
     }
 
 }
